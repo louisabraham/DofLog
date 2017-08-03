0a50a74834764727d03591f121a98208474f63ee
louisabraham <louis.abraham@yahoo.fr>
2017-08-03 20:47:21 +0100

    base code

---
+++ main.py
@@ -0,0 +1,31 @@
+from scapy.all import sniff
+from scapy.all import Raw
+
+from binrw import Data
+from message import Msg
+
+def raw(pa):
+    return bytes(pa.getlayer(Raw))
+
+
+def msgHandler(pa, buf, action):
+    buf += raw(pa)
+    msg = Msg(buf)
+    while msg:
+        action(msg)
+        msg = Msg(buf)
+
+
+def launch(action):
+    print('DofLog is on!\nctrl+c to stop')
+    buf = Data()
+    sniff(filter='tcp port 5555', lfilter=lambda p: p.haslayer(
+        Raw), prn=lambda p: msgHandler(p, buf, action))
+    print('\nDofLog has been stopped!')
+
+
+def action(msg):
+    """
+    action to execute on the message
+    """
+    print(msg.id)
