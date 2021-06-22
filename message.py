dc6293831ed8579e55e4c8457ef620819c7a0c1d
Louis Abraham <louis.abraham@yahoo.fr>
2021-06-22 12:07:54 +0200

    error handling

--- message.py
+++ message.py
@@ -4,16 +4,18 @@ class Msg():
 
     def __init__(self, buf):
         self.b = True
+        pos = buf.pos
         try:
             header = int.from_bytes(buf.read(2), 'big')
             lenData = int.from_bytes(buf.read(header & 3), 'big')
             self.id = header >> 2
             self.data = Data(buf.read(lenData))
         except IndexError:
-            buf.pos = 0
+            buf.pos = pos
             self.b = False
         else:
             buf.end()
 
     def __bool__(self):
         return self.b
+
